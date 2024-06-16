c9c7281fc83b0536e7b79dda7b3803797e9a6812
devipbhoir <140497347+devipbhoir@users.noreply.github.com>
2024-06-16 20:21:43 +0530

    Add files via upload

--- Fire.py
+++ Fire.py
@@ -3,4 +3,6 @@ rf = Roboflow(api_key="pushUxcO4QD23l8yStGo")
 project = rf.workspace().project("fire-ql2gq")
 model=project.version(1).model
 
+
+
 model.predict("a.jpg", confidence=20).save("b.jpg")
